292bfdae76c4ce856ddbd416f09be5ba180d7398
rguillome <dev@guillaumerenaudin.com>
2020-02-20 19:55:01 +0100

    :sparkles: exo 1 à la main

--- solve.py
+++ solve.py
@@ -1,5 +1,11 @@
+import sys
+
+################### READ ######################
+###############################################
+
+file_in = sys.argv[1]
 #A - example
-with open('a_example.txt', 'r') as f:
+with open(file_in+'.txt', 'r') as f:
     lines = f.readlines()
     book_nb, lib_nb, days = lines[0].split()
     scores = lines[1].split()
@@ -10,5 +16,41 @@ with open('a_example.txt', 'r') as f:
         lib_para = lines[l].split()
         ids = lines[l+1].split()
         lib_data.append([lib_para, ids])
-   
+
+################### SOLVE ######################
+###############################################
+
+
+nb_lib = 2
+
+libs = [{
+    'id': 0,
+    'nb_books': 5,
+    'ids': ["0", "1", "2", "3", "4"]
+},
+    {
+    'id': 1,
+    'nb_books': 1,
+    'ids': ["5"]
+}]
+
+
+################### WRITE ######################
+###############################################
+
+
+str_out = '{}\n'.format(nb_lib)
+list_section = ''
+
+for lib in libs:
+    print(lib)
+    list_section += '{} {}\n'.format(lib["id"], lib["nb_books"])
+    list_section += ' '.join(lib["ids"])+'\n'
+
+str_out += list_section
+
+with open('{}_out.txt'.format(file_in), 'w') as file_out:
+    file_out.write(str_out)
+
+
 print(lib_data)
